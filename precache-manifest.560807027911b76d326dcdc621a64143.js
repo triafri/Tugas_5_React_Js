fd0ebc7d136a2c7cf375908cf23c0de055890a2e
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-30 12:23:15 +0700

    Add files via upload

---
+++ precache-manifest.560807027911b76d326dcdc621a64143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7cbc8a64f60607041059cf913d2a505",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d328933b7cfb271eb683",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b2458a03a793be2255e0",
+    "url": "/static/js/2.89bce482.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89bce482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d328933b7cfb271eb683",
+    "url": "/static/js/main.e63c4605.chunk.js"
+  },
+  {
+    "revision": "66674ab69ce1554dc98e",
+    "url": "/static/js/runtime-main.72c1cc57.js"
+  }
+]);
